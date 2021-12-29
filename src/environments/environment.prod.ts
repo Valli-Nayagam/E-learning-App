9311eddce14382fae7c10b2ec17e88c2f9ac0d11
Valli Nayagam <valli@adaptivemedia.in>
2021-12-29 15:54:16 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyC48gjmKCXNx5EzX1L_XzgX2dswuLyX5VA",
+    authDomain: "ecart-service.firebaseapp.com",
+    projectId: "ecart-service",
+    storageBucket: "ecart-service.appspot.com",
+    messagingSenderId: "1063228099999",
+    appId: "1:1063228099999:web:4233d57299a539e26941aa",
+    measurementId: "G-DPX28YCHWZ"
+  }
 };
